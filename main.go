48700ad7c217da5c1ee7266a4719219d053a77f9
Gulácsi Tamás <T.Gulacsi@unosoft.hu>
2024-08-13 08:25:31 +0200

    use os.DirFS, buffer output and shoehorn everything into a <pre>

--- main.go
+++ main.go
@@ -5,19 +5,23 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"flag"
 	"fmt"
 	"html"
 	"io"
+	"io/fs"
 	"log/slog"
 	"net/http"
 	"net/url"
 	"os"
 	"os/signal"
+	"path"
 	"path/filepath"
 	"strings"
 	"syscall"
+	"time"
 
 	"github.com/nxadm/tail"
 	"github.com/tgulacsi/go/httpunix"
@@ -37,12 +41,23 @@ func Main() error {
 	if err != nil {
 		return err
 	}
+	FS := os.DirFS(root)
 
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer cancel()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		dis, err := os.ReadDir(root)
+		p := path.Clean(r.URL.Query().Get("path"))
+		if fi, err := FS.(fs.StatFS).Stat(p); err != nil {
+			slog.Error("stat", "path", p, "root", root, "error", err)
+			p = "/"
+		} else if !fi.Mode().IsDir() {
+			slog.Error("mode", "path", p, "mode", fi.Mode())
+			p = path.Dir(p)
+		}
+
+		slog.Info("/", "path", p)
+		dis, err := FS.(fs.ReadDirFS).ReadDir(p)
 		if len(dis) == 0 && err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -60,18 +75,34 @@ func Main() error {
 <ul>
 `)
 		for _, di := range dis {
-			if di.IsDir() || !di.Type().IsRegular() {
+			bn := di.Name()
+			afn := path.Join(p, bn)
+			if di.Type().IsDir() {
+				io.WriteString(w, "<li><a href=\"./?path="+url.PathEscape(afn)+"\">"+html.EscapeString(bn)+"</a></li>\n")
+			} else if !di.Type().IsRegular() {
 				continue
+			} else {
+				io.WriteString(w, "<li><a href=\"./tail?file="+url.PathEscape(afn)+"\">"+html.EscapeString(bn)+"</a></li>\n")
 			}
-			fmt.Fprintf(w, "<li><a href=\"./tail?file="+url.PathEscape(di.Name())+"\">"+html.EscapeString(di.Name())+"</a></li>\n")
 		}
 		io.WriteString(w, `
 	</ul></p>
 </body>
 </html>`)
 	})
+
 	http.HandleFunc("GET /tail", func(w http.ResponseWriter, r *http.Request) {
-		fn := r.URL.Query().Get("file")
+		fn := path.Clean(r.URL.Query().Get("file"))
+		if fi, err := FS.(fs.StatFS).Stat(fn); err != nil {
+			slog.Error("stat", "file", fn, "error", err)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		} else if !fi.Mode().IsRegular() {
+			slog.Error("not regular", "file", fn, "mode", fi.Mode())
+			http.Error(w, fmt.Sprintf("%q is not a regular file (%v)", fn, fi), http.StatusBadRequest)
+			return
+		}
+
 		w.Header().Set("Content-Type", "text/html")
 		w.WriteHeader(200)
 		io.WriteString(w, `<!DOCTYPE html>
@@ -84,30 +115,43 @@ func Main() error {
     </head>
     <body>
         <h1>`+html.EscapeString(fn)+`</h1>
-        <div hx-ext="sse" sse-connect="/tail-sse?wrap=pre&file=`+
+        <pre hx-ext="sse" sse-connect="/tail-sse?left=&right=<br>&file=`+
 			url.QueryEscape(fn)+
-			`" sse-swap="message" hx-swap="afterend swap:1s">
-        </div>
+			`" sse-swap="message" hx-swap="afterbegin swap:1s">
+        </pre>
     </body>
 </html>`)
 	})
 
 	http.HandleFunc("/tail-sse", func(w http.ResponseWriter, r *http.Request) {
-		wrap := r.URL.Query().Get("wrap")
-		fn, err := filepath.Abs(filepath.Join(root, r.URL.Query().Get("file")))
-		if err != nil {
+		q := r.URL.Query()
+		left := q.Get("left")
+		right := q.Get("right")
+		fn := path.Clean(q.Get("file"))
+		if fi, err := FS.(fs.StatFS).Stat(fn); err != nil {
+			slog.Error("stat", "file", fn, "root", root, "error", err)
 			http.Error(w, err.Error(), http.StatusNotFound)
 			return
-		}
-		slog.Info("tail", "URL", r.URL, "method", r.Method)
-		if !strings.HasPrefix(fn, root) {
-			http.Error(w, fmt.Sprintf("only files under %q can be tailed", root), http.StatusBadRequest)
+		} else if !fi.Mode().IsRegular() {
+			slog.Error("not regular", "file", fn, "root", root, "mode", fi.Mode())
+			http.Error(w, fmt.Sprintf("%q is not a regular file (%v)", fn, fi.Mode()), http.StatusBadRequest)
 			return
 		}
-		tl, err := tail.TailFile(fn, tail.Config{
+
+		slog.Info("tail", "URL", r.URL, "method", r.Method, "file", fn)
+		afn, err := filepath.Abs(filepath.Join(root, filepath.FromSlash(fn)))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		if !strings.HasPrefix(afn, root) {
+			http.Error(w, fmt.Sprintf("only files under %q can be tailed (%q)", root, afn), http.StatusBadRequest)
+			return
+		}
+		tl, err := tail.TailFile(afn, tail.Config{
 			ReOpen: true, Follow: true,
 			MustExist:     true,
-			Poll:          true,
+			Poll:          true, // otherwise close/reopen fails
 			CompleteLines: true,
 		})
 		if err != nil {
@@ -127,18 +171,29 @@ func Main() error {
 		w.Header().Set("Cache-Control", "no-cache")
 		w.Header().Set("Connection", "keep-alive")
 
+		ticker := time.NewTicker(time.Second)
+		defer ticker.Stop()
+		bw := bufio.NewWriter(w)
 		ctx := r.Context()
 		// Create a channel to send data
 		for {
 			select {
 			case <-ctx.Done():
 				return
+
 			case line := <-tl.Lines:
-				if wrap != "" {
-					io.WriteString(w, "data: <"+wrap+">"+html.EscapeString(line.Text)+"</"+wrap+">\n\n")
+				bw.WriteString("data: ")
+				if left == "" && right == "" {
+					bw.WriteString(line.Text)
 				} else {
-					io.WriteString(w, "data: "+line.Text+"\n\n")
+					bw.WriteString(left)
+					bw.WriteString(html.EscapeString(line.Text))
+					bw.WriteString(right)
 				}
+				bw.WriteString("\n\n")
+
+			case <-ticker.C:
+				bw.Flush()
 				fl.Flush()
 			}
 		}
